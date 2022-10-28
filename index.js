3e846b38a34d68e5601fc737e48cef5a86b40581
Sarah-Albalawi <s.albalawi@outlook.com>
2022-10-28 09:05:05 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    return cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+    return cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(){
+   let newCat;
+    return newCat = [...cats,'Broom'];
+      
+}
+
+function prependCat(){
+         let copyCat;
+         return copyCat= ['Arnold', ...cats];
+}
+function removeFirstCat(){
+    
+    return cats.slice(1);
+}
+function removeLastCat(){
+   return cats.slice(0,2);
+}
